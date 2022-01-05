2260ca8f3fb0e4ca16ebd50df334d109268d9c23
Daymon <NotDaymon@users.noreply.github.com>
2022-01-05 14:05:29 -0600

    DayThree part 1

--- src/DayThree/source.kt
+++ src/DayThree/source.kt
@@ -1,6 +1,7 @@
 package DayThree
 
 import java.io.File
+import kotlin.math.pow
 
 fun main(){
     val input = File("./src/DayThree/input.txt").readLines()
@@ -8,8 +9,43 @@ fun main(){
     println(partTwo(input))
 }
 
+class BitCounter(val bits: IntArray){
+    fun processLine(line: String){
+        for(index in line.indices){
+            bits[index] += if(line[index] == '1') 1 else -1
+        }
+    }
+
+    fun convertToBinary(): BitCounter =
+        this.apply {
+            bits.onEachIndexed { index, it ->
+                bits[index] = if (it > 0) 1 else 0
+            }
+        }
+
+    fun flip(): BitCounter =
+        this.apply {
+            bits.onEachIndexed { index, it ->
+                bits[index] = if(it == 1) 0 else 1
+            }
+        }
+
+
+    fun toNumber(): Int =
+        bits.reversedArray().foldIndexed(0) { index, acc, i ->
+            acc + (i * (2.0).pow(index.toDouble()).toInt())
+        }
+
+}
+
 fun partOne(input: List<String>): Int{
-    return -1
+    val counter = BitCounter(IntArray(12){ 0 })
+    for(line in input){
+        counter.processLine(line)
+    }
+    counter.convertToBinary()
+
+    return counter.toNumber() * counter.flip().toNumber()
 }
 
 fun partTwo(input: List<String>): Int{
