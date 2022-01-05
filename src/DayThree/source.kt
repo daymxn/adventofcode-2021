f869788bb81a4679d650a51b998533e33cdc3f4e
Daymon <NotDaymon@users.noreply.github.com>
2022-01-05 12:58:13 -0600

    Starting DayThree

---
+++ src/DayThree/source.kt
@@ -0,0 +1,17 @@
+package DayThree
+
+import java.io.File
+
+fun main(){
+    val input = File("./src/DayThree/input.txt").readLines()
+    println(partOne(input))
+    println(partTwo(input))
+}
+
+fun partOne(input: List<String>): Int{
+    return -1
+}
+
+fun partTwo(input: List<String>): Int{
+    return -1
+}
